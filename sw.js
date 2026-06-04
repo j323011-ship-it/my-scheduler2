fdb7723c03851c4a7dbe84481ee96698ceca97bd
j323011-ship-it <j323011@g.kogakuin.jp>
2026-06-04 13:52:40 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,21 @@
-const CACHE = 'scheduler-v1';
-const ASSETS = [
-  './',
+const CACHE_NAME = 'scheduler-v1'
+const FILES = [
   './index.html',
   './style.css',
-  './app.js',
-  './manifest.json',
-];
+  './script.js',
+  './manifest.json'
+]
 
+// インストール時にキャッシュ
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS))
-  );
-});
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+  )
+})
 
+// オフラインでもキャッシュから返す
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
-});
+    caches.match(e.request).then(res => res || fetch(e.request))
+  )
+})
